42d2afe06b0e44a4989c35a95191aa2f40361ba3
juacomiranda <jmiranda@live.cl>
2024-12-15 11:01:07 -0300

    d-inline

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -5,10 +5,10 @@ import  logo  from "../../img/electricidad.png";
 export const Navbar = () => {
 	return (
         <div className="container-fluid">
-        <nav className="navbar navbar-expand-sm fixed-top bg-body-tertiary">
+        <nav className="navbar fixed-top bg-body-tertiary">
             <div className="container-fluid mx-5">
                 <a className="navbar-brand" href="#">
-                    <img className="img" src={logo} alt="Electricidad G&G" />
+                    <img className="img d-inline-block align-text-top" src={logo} alt="Electricidad G&G" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
