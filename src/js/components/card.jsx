1e918dcf174bd3c111b1b0794f530c248789eb4a
juacomiranda <jmiranda@live.cl>
2024-12-25 11:04:49 -0300

    img_fluid_card

--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -5,10 +5,10 @@ import React from "react";
 export const Card = (props) => {
     return (
         <div className="card m-2">
-            <img src={props.imagen} className="card-img-top" alt="rayo" />
-            <div className="card-body">
+            <img src={props.imagen} className="card-img-top img-fluid m-auto" alt="rayo" />
+            <div className="card-body text-center">
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.texto}</p>
+                <p className="card-text m-auto">{props.texto}</p>
                 <a href="#" className="btn btn-primary">Detalle</a>
             </div>
         </div>
