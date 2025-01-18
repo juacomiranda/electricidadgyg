2e0a88ff3868cb8dc3f78fbb1647b2f21ee4ea50
juacomiranda <jmiranda@live.cl>
2025-01-18 19:32:40 -0300

    formspree_react

--- src/js/views/contacto.jsx
+++ src/js/views/contacto.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useForm } from '@formspree/react';
+
+
+    
 
 export const Contacto = () => {
+    const [state, handleSubmit] = useForm("movvzpjv");
+    if (state.succeeded) {
+        return <p>Te contactaremos pronto!</p>;
+    }
+    
     return (
         <div className="container mt-5 pt-5" id="contacto">
             <h2 className="text-center  ">¿Quieres ponerte en contacto con nosotros?</h2>
             <div className="container bg-contacto">
-                <form action="https://formspree.io/f/movvzpjv" method="POST" id="contactform" className="bg-light py-2">
+                <form action="https://formspree.io/f/movvzpjv" method="POST" id="contactform" className="bg-light py-2" onSubmit={handleSubmit}>
                     <div className="mb-3 mx-2 mt-2">
                         <input type="text" className="form-control" id="nombre" placeholder="Nombre"/>
                     </div>
@@ -18,7 +27,7 @@ export const Contacto = () => {
                     <div className="mb-3 mx-2">
                         <textarea className="form-control" id="message" rows="4" required />
                     </div>
-                    <button type="submit" class="btn btn-danger ms-3">Enviar</button>
+                    <button type="submit" disabled={state.submitting} class="btn btn-danger ms-3">Enviar</button>
                 </form>
             </div>
         </div>
